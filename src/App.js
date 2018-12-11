a9d8bb8f034f20501f87364395256dca2235fecd
Roloffqt <mads@roloff-design.dk>
2018-12-11 11:25:48 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,35 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <div className="App-Overlay">
+        
+        
+
+        <h1 className="App-Title">
+           MIB - Summermeet 2019
+         </h1>
+
+        <div className="App-ImgCon">
+          <div className="App-ImgSection-1">
+          </div>
+          <div className="App-ImgSection-2">
+          </div>
+          <div className="App-ImgSection-3">
+          </div>
+        </div>
+         <h2 className="App-Date">
+           5-7 July 2019
+         </h2>
+         <div className="App-Tickets">
+
+         <h4>Get your Tickets here</h4>
+
+         <button className="App-button">
+          Tickets
+         </button>
+         
+         </div>
+        </div>
         </header>
       </div>
     );
